b03ab3ecbb2bd64bdb6739dba353be73c5a8b4f3
Caleb <calebjun02@gmail.com>
2026-03-24 12:27:24 -0400

    Add LibraryDataServerApplication.java

---
+++ src/main/java/sheridan/junc/librarydata/LibraryDataServerApplication.java
@@ -0,0 +1,12 @@
+package sheridan.junc.librarydata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryDataServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryDataServerApplication.class, args);
+    }
+}
