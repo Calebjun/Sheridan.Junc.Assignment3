e2df8130716efbf504421cb412af3897759c5520
Caleb <calebjun02@gmail.com>
2026-03-24 22:29:58 -0400

    Add ApiExceptionHandler.java

---
+++ src/main/java/sheridan/junc/librarydata/errors/ApiExceptionHandler.java
@@ -0,0 +1,46 @@
+package sheridan.junc.librarydata.errors;
+
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@Slf4j
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ApiResponse(content = @Content(mediaType = "application/json"))
+    public ApiError handleIllegalArgumentException(IllegalArgumentException ex) {
+        log.error(ex.getMessage(), ex);
+        return new ApiError(HttpStatus.BAD_REQUEST.value(), ex.getClass().getSimpleName(), ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ApiResponse(content = @Content(mediaType = "application/json"))
+    public ApiError handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException ex) {
+        log.error(ex.getMessage(), ex);
+        return new ApiError(HttpStatus.BAD_REQUEST.value(), ex.getClass().getSimpleName(), ex.getMessage());
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ApiResponse(content = @Content(mediaType = "application/json"))
+    public ApiError handleNoResourceFoundException(NoResourceFoundException ex) {
+        log.error(ex.getMessage(), ex);
+        return new ApiError(HttpStatus.NOT_FOUND.value(), ex.getClass().getSimpleName(), "No resource found at " + ex.getResourcePath());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ApiResponse(content = @Content(mediaType = "application/json"))
+    public ApiError handleAllUncaughtException(Exception ex) {
+        log.error(ex.getMessage(), ex);
+        return new ApiError(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getClass().getSimpleName(), ex.getMessage());
+    }
+}
