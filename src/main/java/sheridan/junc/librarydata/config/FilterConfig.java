b335ab024c1c10c7417cac30106b416b209c8cc5
Caleb <calebjun02@gmail.com>
2026-03-25 01:36:39 -0400

    Add CorsConfig, OpenApiConfig, and FilterConfig

---
+++ src/main/java/sheridan/junc/librarydata/config/FilterConfig.java
@@ -0,0 +1,23 @@
+package sheridan.junc.librarydata.config;
+
+import sheridan.junc.librarydata.filter.RequestLogFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FilterConfig {
+
+    @Bean
+    public FilterRegistrationBean<RequestLogFilter> loggingFilter() {
+        FilterRegistrationBean<RequestLogFilter> registrationBean = new FilterRegistrationBean<>();
+
+        registrationBean.setFilter(new RequestLogFilter());
+        // Specify the URL patterns
+        registrationBean.addUrlPatterns("/api/*");
+        // Set the order if you have multiple filters
+        registrationBean.setOrder(1);
+
+        return registrationBean;
+    }
+}
